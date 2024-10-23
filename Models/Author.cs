ccc51d106a515e24b9048073c072467bab9ba6dd
andreikish <andreicezar.kish@gmail.com>
2024-10-23 17:05:12 +0300

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using Kish_AndreiCezar_Lab2.Models;
+
+namespace Kish_AndreiCezar_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
